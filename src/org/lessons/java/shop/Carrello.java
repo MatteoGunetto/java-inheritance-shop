95ab8dc30cc1227332729057d0b3a1283e718ffd
MatteoGunetto <matteogunetto@tiscali.it>
2023-10-23 15:55:02 +0200

    esercizio completo senza bonus

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -5,7 +5,66 @@ import java.util.Scanner;
 
 public class Carrello {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Prodotto> carrello = new ArrayList<>();
 
+        while (true) {
+            System.out.println("Scegli il tipo di prodotto da inserire ((1)Smartphone, (2)Televisore, (3)Cuffie, (0)Fine):");
+            int scelta = scanner.nextInt();
+            scanner.nextLine();
+
+            if (scelta == 0) {
+                break;
+            }
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+            System.out.print("Descrizione: ");
+            String descrizione = scanner.nextLine();
+            System.out.print("Prezzo: ");
+            double prezzo = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("IVA: ");
+            int iva = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (scelta) {
+                case 1:
+                    System.out.print("IMEI: ");
+                    String imei = scanner.nextLine();
+                    System.out.print("Memoria (GB): ");
+                    int memoria = scanner.nextInt();
+                    carrello.add(new Smartphone( nome, descrizione, prezzo, iva, imei, memoria));
+                    break;
+                case 2:
+                    System.out.print("Dimensioni (pollici): ");
+                    int dimensioni = scanner.nextInt();
+                    System.out.print("é Smart (true/false): ");
+                    boolean isSmart = scanner.nextBoolean();
+                    carrello.add(new Televisore( nome, descrizione, prezzo, iva, dimensioni, isSmart));
+                    break;
+                case 3:
+                    System.out.print("Colore: ");
+                    String colore = scanner.nextLine();
+                    System.out.print("sono Wireless(true/false): ");
+                    boolean isWireless = scanner.nextBoolean();
+                    carrello.add(new Cuffie( nome, descrizione, prezzo, iva, colore, isWireless));
+                    break;
+            }
+        }
+
+        // Stampa il carrello
+        System.out.println("Carrello:");
+        for (Prodotto prodotto : carrello) {
+            System.out.println(prodotto);
+        }
+
+        // Calcola il totale
+        double totale = 0;
+        for (Prodotto prodotto : carrello) {
+            totale += prodotto.getPrezzoConIva();
+        }
+        System.out.println("Totale: " + totale);
+
+        scanner.close();
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,6 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
 public class Prodotto {
     private int codice;
     private String nome;
@@ -7,16 +8,17 @@ public class Prodotto {
     private double prezzo;
     private int iva;
 
-    public Prodotto(int codice, String nome, String descrizione, double prezzo, int iva) {
-        this.codice = codice;
+    public Prodotto(String nome, String descrizione, double prezzo, int iva) {
+        this.codice = codiceRandom();
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.iva = iva;
     }
 
-    public int getCodice() {
-        return codice;
+    private int codiceRandom() {
+        Random random = new Random();
+        return random.nextInt(10000);
     }
 
     public String getNome() {
@@ -34,5 +36,66 @@ public class Prodotto {
     public double getPrezzoConIva() {
         return prezzo + (prezzo * iva / 100);
     }
+
+    // Override del metodo toString \n per mandare a capo
+    @Override
+    public String toString() {
+        return "Codice: " + codice + "\nNome: " + nome + "\nDescrizione: " + descrizione + "\nPrezzo Base: " + prezzo + "\nPrezzo con IVA: " + getPrezzoConIva();
+    }
 }
 
+//Smartphone
+class Smartphone extends Prodotto {
+    private String imei;
+    private int memoria;
+
+    public Smartphone( String nome, String descrizione, double prezzo, int iva, String imei, int memoria) {
+        super(nome, descrizione, prezzo, iva);
+        this.imei = imei;
+        this.memoria = memoria;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nIMEI: " + imei + "\nMemoria: " + memoria + " GB";
+    }
+}
+
+// Televisori
+class Televisore extends Prodotto {
+    private int dimensioni;
+    private boolean tipologia;
+
+    public Televisore( String nome, String descrizione, double prezzo, int iva, int dimensioni, boolean tipologia) {
+        super( nome, descrizione, prezzo, iva);
+        this.dimensioni = dimensioni;
+        this.tipologia = tipologia;
+    }
+
+
+    @Override
+    public String toString() {
+        String smartStatus = tipologia ? "Smart" : "Non Smart";
+        return super.toString() + "\nDimensioni: " + dimensioni + " pollici\nTipo: " + smartStatus;
+    }
+}
+
+// Cuffie
+class Cuffie extends Prodotto {
+    private String colore;
+    private boolean connessione;
+
+    public Cuffie( String nome, String descrizione, double prezzo, int iva, String colore, boolean connessione) {
+        super( nome, descrizione, prezzo, iva);
+        this.colore = colore;
+        this.connessione = connessione;
+    }
+
+
+    @Override
+    public String toString() {
+        String tipo = connessione ? "Wireless" : "Cablate";
+        return super.toString() + "\nColore: " + colore + "\nConnessione: " + tipo;
+    }
+}

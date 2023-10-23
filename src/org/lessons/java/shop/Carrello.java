47075c82a3f4920854b542624ca0474b2620241a
MatteoGunetto <matteogunetto@tiscali.it>
2023-10-23 17:51:59 +0200

    esercizio completo

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -1,70 +1,78 @@
 package org.lessons.java.shop;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
-public class Carrello {
+class Carrello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Prodotto> carrello = new ArrayList<>();
+        double totale = 0;
+
+        System.out.print("Hai una tessera fedeltà? (si/no): ");
+        boolean tesseraFedelta = scanner.nextLine().equalsIgnoreCase("si");
 
         while (true) {
-            System.out.println("Scegli il tipo di prodotto da inserire ((1)Smartphone, (2)Televisore, (3)Cuffie, (0)Fine):");
-            int scelta = scanner.nextInt();
-            scanner.nextLine();
-
-            if (scelta == 0) {
+            System.out.print("Inserisci un prodotto (Smartphone/Televisore/Cuffie/esci): ");
+            String scelta = scanner.nextLine();
+            if (scelta.equalsIgnoreCase("esci")) {
                 break;
             }
-            System.out.print("Nome: ");
-            String nome = scanner.nextLine();
-            System.out.print("Descrizione: ");
-            String descrizione = scanner.nextLine();
-            System.out.print("Prezzo: ");
-            double prezzo = scanner.nextDouble();
-            scanner.nextLine();
-            System.out.print("IVA: ");
-            int iva = scanner.nextInt();
-            scanner.nextLine();
 
-            switch (scelta) {
-                case 1:
-                    System.out.print("IMEI: ");
-                    String imei = scanner.nextLine();
-                    System.out.print("Memoria (GB): ");
-                    int memoria = scanner.nextInt();
-                    carrello.add(new Smartphone( nome, descrizione, prezzo, iva, imei, memoria));
-                    break;
-                case 2:
-                    System.out.print("Dimensioni (pollici): ");
-                    int dimensioni = scanner.nextInt();
-                    System.out.print("é Smart (true/false): ");
-                    boolean isSmart = scanner.nextBoolean();
-                    carrello.add(new Televisore( nome, descrizione, prezzo, iva, dimensioni, isSmart));
-                    break;
-                case 3:
-                    System.out.print("Colore: ");
-                    String colore = scanner.nextLine();
-                    System.out.print("sono Wireless(true/false): ");
-                    boolean isWireless = scanner.nextBoolean();
-                    carrello.add(new Cuffie( nome, descrizione, prezzo, iva, colore, isWireless));
-                    break;
+            String nome;
+            String descrizione;
+            double prezzo;
+            double iva;
+
+
+
+            System.out.print("Nome: ");
+            nome = scanner.nextLine();
+
+            System.out.print("Descrizione: ");
+            descrizione = scanner.nextLine();
+
+            System.out.print("Prezzo: ");
+            prezzo = Double.parseDouble(scanner.nextLine());
+
+            System.out.print("Iva: ");
+            iva = Double.parseDouble(scanner.nextLine());
+
+            Prodotto prodotto = null;
+
+            if (scelta.equalsIgnoreCase("Smartphone")) {
+                System.out.print("IMEI: ");
+                String imei = scanner.nextLine();
+
+                System.out.print("Memoria (GB): ");
+                int memoriaGB = Integer.parseInt(scanner.nextLine());
+
+                prodotto = new Smartphone( nome, descrizione, prezzo, iva, imei, memoriaGB);
+            }
+            else if (scelta.equalsIgnoreCase("Televisore")) {
+                System.out.print("Dimensioni: ");
+                String dimensioni = scanner.nextLine();
+
+                System.out.print("Smart TV (si/no): ");
+                boolean isSmart = scanner.nextLine().equalsIgnoreCase("si");
+
+                prodotto = new Televisore( nome, descrizione, prezzo, iva, dimensioni, isSmart);
+            } else if (scelta.equalsIgnoreCase("Cuffie")) {
+                System.out.print("Colore: ");
+                String colore = scanner.nextLine();
+
+                System.out.print("Wireless (si/no): ");
+                boolean isWireless = scanner.nextLine().equalsIgnoreCase("si");
+
+                prodotto = new Cuffie( nome, descrizione, prezzo, iva, colore, isWireless);
+            }
+
+            if (prodotto != null) {
+                totale += tesseraFedelta ? prodotto.getPrezzoScontato(tesseraFedelta) : prodotto.getPrezzo();
+                System.out.println("Prodotto aggiunto al carrello:\n" + prodotto.toString());
             }
         }
 
-        // Stampa il carrello
-        System.out.println("Carrello:");
-        for (Prodotto prodotto : carrello) {
-            System.out.println(prodotto);
-        }
-
-        // Calcola il totale
-        double totale = 0;
-        for (Prodotto prodotto : carrello) {
-            totale += prodotto.getPrezzoConIva();
-        }
-        System.out.println("Totale: " + totale);
-
+        System.out.println("Totale: " + String.format("%.2f", totale));
         scanner.close();
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,14 +1,15 @@
 package org.lessons.java.shop;
 
+
 import java.util.Random;
-public class Prodotto {
-    private int codice;
+class Prodotto {
+    private final int codice;
     private String nome;
     private String descrizione;
     private double prezzo;
-    private int iva;
+    private double iva;
 
-    public Prodotto(String nome, String descrizione, double prezzo, int iva) {
+    public Prodotto(String nome, String descrizione, double prezzo, double iva) {
         this.codice = codiceRandom();
         this.nome = nome;
         this.descrizione = descrizione;
@@ -25,11 +26,19 @@ public class Prodotto {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getDescrizione() {
         return descrizione;
     }
 
-    public double getPrezzoBase() {
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
         return prezzo;
     }
 
@@ -37,65 +46,92 @@ public class Prodotto {
         return prezzo + (prezzo * iva / 100);
     }
 
+    public double getPrezzoScontato(boolean tesseraFedelta) {
+        double sconto = tesseraFedelta ? 0.02 : 0.0;
+        return prezzo - (prezzo * sconto);
+    }
     // Override del metodo toString \n per mandare a capo
     @Override
     public String toString() {
-        return "Codice: " + codice + "\nNome: " + nome + "\nDescrizione: " + descrizione + "\nPrezzo Base: " + prezzo + "\nPrezzo con IVA: " + getPrezzoConIva();
+        return "Codice: " + codice +
+                "\nNome: " + nome +
+                "\nDescrizione: " + descrizione +
+                "\nPrezzo: " + prezzo +
+                "\nIva: " + iva;
     }
 }
 
-//Smartphone
 class Smartphone extends Prodotto {
     private String imei;
-    private int memoria;
+    private int memoriaGB;
 
-    public Smartphone( String nome, String descrizione, double prezzo, int iva, String imei, int memoria) {
-        super(nome, descrizione, prezzo, iva);
+    public Smartphone( String nome, String descrizione, double prezzo, double iva, String imei, int memoriaGB) {
+        super( nome, descrizione, prezzo, iva);
         this.imei = imei;
-        this.memoria = memoria;
+        this.memoriaGB = memoriaGB;
     }
 
+    @Override
+    public double getPrezzoScontato(boolean tesseraFedelta) {
+        double scontoBase = tesseraFedelta ? 0.02 : 0.0;
+        double scontoMemoria = (memoriaGB < 32) ? 0.0 : 0.05;
+        return getPrezzo() - (getPrezzo() * (scontoBase + scontoMemoria));
+    }
 
     @Override
     public String toString() {
-        return super.toString() + "\nIMEI: " + imei + "\nMemoria: " + memoria + " GB";
+        return super.toString() +
+                "\nIMEI: " + imei +
+                "\nMemoria: " + memoriaGB + "GB";
     }
 }
 
-// Televisori
 class Televisore extends Prodotto {
-    private int dimensioni;
-    private boolean tipologia;
+    private String dimensioni;
+    private boolean smart;
 
-    public Televisore( String nome, String descrizione, double prezzo, int iva, int dimensioni, boolean tipologia) {
+    public Televisore( String nome, String descrizione, double prezzo, double iva, String dimensioni, boolean smart) {
         super( nome, descrizione, prezzo, iva);
         this.dimensioni = dimensioni;
-        this.tipologia = tipologia;
+        this.smart = smart;
     }
 
+    @Override
+    public double getPrezzoScontato(boolean tesseraFedelta) {
+        double scontoBase = tesseraFedelta ? 0.02 : 0.0;
+        double scontoSmart = (smart) ? 0.0 : 0.10;
+        return getPrezzo() - (getPrezzo() * (scontoBase + scontoSmart));
+    }
 
     @Override
     public String toString() {
-        String smartStatus = tipologia ? "Smart" : "Non Smart";
-        return super.toString() + "\nDimensioni: " + dimensioni + " pollici\nTipo: " + smartStatus;
+        return super.toString() +
+                "\nDimensioni: " + dimensioni +
+                "\nSmart TV: " + (smart ? "Si" : "No");
     }
 }
 
-// Cuffie
 class Cuffie extends Prodotto {
     private String colore;
     private boolean connessione;
 
-    public Cuffie( String nome, String descrizione, double prezzo, int iva, String colore, boolean connessione) {
+    public Cuffie( String nome, String descrizione, double prezzo, double iva, String colore, boolean connessione) {
         super( nome, descrizione, prezzo, iva);
         this.colore = colore;
         this.connessione = connessione;
     }
 
+    @Override
+    public double getPrezzoScontato(boolean tesseraFedelta) {
+        double scontoBase = tesseraFedelta ? 0.02 : 0.0;
+        double scontoCablate = (connessione) ? 0.0 : 0.07 ;
+        return getPrezzo() - (getPrezzo() * (scontoBase + scontoCablate));
+    }
 
     @Override
     public String toString() {
-        String tipo = connessione ? "Wireless" : "Cablate";
-        return super.toString() + "\nColore: " + colore + "\nConnessione: " + tipo;
+        return super.toString() +
+                "\nColore: " + colore +
+                "\nWireless: " + (connessione ? "Si" : "No");
     }
 }
